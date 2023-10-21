d1a11e4e77bc9d368b739a2233234cc90515bc96
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-10-21 11:51:18 +0200

    units

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -224,8 +224,8 @@ const Home = () => {
         >
           <Column align={"center"} field="power_meter_name" header="Powermeter name"></Column>
           <Column align={"center"} field="channel_name" header="Channel"></Column>
-          <Column align={"center"} field="avg" header="AVG"></Column>
-          <Column align={"center"} field="summ" header="Summ"></Column>
+          <Column align={"center"} field="avg" header="AVG (Wh)"></Column>
+          <Column align={"center"} field="summ" header="Summ (Wh)"></Column>
         </DataTable>
       </div>
     </div>
